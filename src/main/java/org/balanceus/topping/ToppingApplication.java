24f6154bb00f45ccd1ffd3c333e29049568667cd
singingsandhill <jisu0259@naver.com>
2025-07-02 17:55:57 +0900

    refactor: db setting - rollback

--- src/main/java/org/balanceus/topping/ToppingApplication.java
+++ src/main/java/org/balanceus/topping/ToppingApplication.java
@@ -2,11 +2,17 @@ package org.balanceus.topping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ToppingApplication {
 
 	public static void main(String[] args) {
+
+		// Dotenv dotenv = Dotenv.configure().load();
+		// dotenv.entries().forEach(entry ->
+		// 	System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(ToppingApplication.class, args);
 	}
 
