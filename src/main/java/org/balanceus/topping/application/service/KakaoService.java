f5b97c72838d9f0c224a4e606220cefc5c6476e9
singingsandhill <jisu0259@naver.com>
2025-08-24 17:26:22 +0900

    fix: 소셜 로그인 리다이렉트 url 수정

--- src/main/java/org/balanceus/topping/application/service/KakaoService.java
+++ src/main/java/org/balanceus/topping/application/service/KakaoService.java
@@ -101,7 +101,7 @@ public class KakaoService {
 		MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
 		body.add("grant_type", "authorization_code");
 		body.add("client_id", kakaoRestApiKey);
-		body.add("redirect_uri", "http://topping.cloud/api/user/kakao/callback");
+		body.add("redirect_uri", "http://35.231.208.65:8080/api/user/kakao/callback");
 		body.add("code", code);
 
 		RequestEntity<MultiValueMap<String, String>> requestEntity = RequestEntity
