a7793e052364310da9664a864403dce7a4c71ee8
singingsandhill <jisu0259@naver.com>
2025-09-02 06:59:14 +0900

    fix: 시간 설정 한국으로

--- src/main/java/org/balanceus/topping/ToppingApplication.java
+++ src/main/java/org/balanceus/topping/ToppingApplication.java
@@ -1,5 +1,7 @@
 package org.balanceus.topping;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import io.github.cdimascio.dotenv.Dotenv;
@@ -8,6 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToppingApplication {
 
 	public static void main(String[] args) {
+
+		// Set default timezone to Korea
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 
 		// Dotenv dotenv = Dotenv.configure().load();
 		// dotenv.entries().forEach(entry ->
